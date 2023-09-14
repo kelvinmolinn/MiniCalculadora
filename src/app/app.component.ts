ce2598430ef9476e1f31341a93b052d6aa448ba1
Kelvin Molina <kelvinmolinn@gmail.com>
2023-09-13 19:59:38 -0600

    primer  commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MiniCalculadoraApp';
+  numero1: number=0;
+  numero2: number=0;
+  resultado: number=0;
+
+  sumar(): void{
+    this.resultado= this.numero1+this.numero2;
+
+  }
+  restar(): void{
+    this.resultado= this.numero1-this.numero2;
+    
+  }
+  multiplicar(): void{
+    this.resultado= this.numero1*this.numero2;
+    
+  }
+  dividir(): void{
+    this.resultado= this.numero1/this.numero2;
+    
+  }
+
 }
